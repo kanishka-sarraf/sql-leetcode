b4ee89fd6a95ca866ea0a9af13829046ed036145
kanishka-sarraf <kanishkasarraf20@gmail.com>
2025-09-04 12:36:17 +0530

    Update 02-Data Science Skills.sql

--- datalemur/1 - Easy/02-Data Science Skills.sql
+++ datalemur/1 - Easy/02-Data Science Skills.sql
@@ -1,3 +1,7 @@
+-- You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
+-- Write a query to list the candidates who possess all of the required skills for the job. Sort the output by candidate ID in ascending order.
+
+-- solution1
 SELECT candidate_id
 FROM   (SELECT candidate_id,
                Group_concat(skill) AS skill_set
@@ -7,3 +11,11 @@ FROM   (SELECT candidate_id,
                AND skill_set LIKE '%Tableau%'
                AND skill_set LIKE '%PostgreSQL%') t
 ORDER  BY candidate_id ASC; 
+
+-- solution2
+SELECT candidate_id
+FROM candidates
+WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(skill) = 3
+ORDER BY candidate_id;
