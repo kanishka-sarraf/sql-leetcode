5fdf1cd8b615ccc24f1a1f319362406d3df71c50
kanishka-sarraf <kanishkasarraf20@gmail.com>
2025-09-05 16:45:09 +0530

    Time: 259 ms (35.24%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/*
 select 
     t.machine_id, 
     round(avg(end_time-start_time),3) as processing_time
@@ -11,3 +12,17 @@ from activity
 group by machine_id, process_id
 ) t
 group by t.machine_id;
+*/
+
+select 
+    machine_id,
+    round(avg(end_time-start_time),3) as processing_time
+from (
+select 
+machine_id, process_id, timestamp as end_time, 
+lag(timestamp,1) over(partition by machine_id, process_id 
+                            order by timestamp asc) as start_time
+from activity
+) t 
+where t.start_time is not null
+group by machine_id;
