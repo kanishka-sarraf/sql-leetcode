ca5aa49ec6ed789c1b7721b74e224f62d8977be6
kanishka-sarraf <kanishkasarraf20@gmail.com>
2025-09-05 11:35:37 +0530

    Time: 2353 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/*
 with first_quater_product as (
 select product_id, count(*) as num_prod
 from sales s1
@@ -11,3 +12,13 @@ having num_prod = (select count(*)
 select p.product_id, p.product_name
 from first_quater_product t
 join product p on t.product_id = p.product_id;
+*/
+
+select s1.product_id, p.product_name-- , count(*) as num_prod
+from sales s1
+join product p on s1.product_id = p.product_id
+where s1.sale_date between '2019-01-01' and '2019-03-31'
+group by s1.product_id, p.product_name
+having count(*) = (select count(*) 
+                   from sales s2 
+                   where s2.product_id = s1.product_id)
