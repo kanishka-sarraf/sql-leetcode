22cc4092d95adf538d13f826e90badc9dc7442fc
kanishka-sarraf <kanishkasarraf20@gmail.com>
2025-09-05 18:57:09 +0530

    Time: 582 ms (41.15%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,14 +1,11 @@
 # Write your MySQL query statement below
-select t.employee_id 
-from (
-    select e.employee_id
-    from employees e
-    left join salaries s on e.employee_id = s.employee_id
-    where s.salary is null
-    union 
-    select s.employee_id
-    from employees e
-    right join salaries s on e.employee_id = s.employee_id
-    where e.name is null
-) t 
-order by t.employee_id asc;
+select e.employee_id
+from employees e
+left join salaries s on e.employee_id = s.employee_id
+where s.salary is null
+union 
+select s.employee_id
+from employees e
+right join salaries s on e.employee_id = s.employee_id
+where e.name is null
+order by employee_id asc;
