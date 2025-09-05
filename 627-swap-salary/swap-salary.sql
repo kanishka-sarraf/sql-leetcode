0c088b8c558674b78e2b686154584a8fee073b79
kanishka-sarraf <kanishkasarraf20@gmail.com>
2025-09-05 11:00:30 +0530

    Time: 217 ms (97.34%) | Memory: 0B (100.00%) - LeetSync

--- 627-swap-salary/swap-salary.sql
+++ 627-swap-salary/swap-salary.sql
@@ -1,5 +1,13 @@
 # Write your MySQL query statement below
+/*
 with swap_sex_cte as
 (select *, case when sex = 'm' then 'f' else 'm' end as swap_sex from salary)
 update salary s
 set s.sex = (select swap_sex from swap_sex_cte c where c.id = s.id);
+*/
+
+UPDATE salary SET sex =
+CASE sex
+    WHEN 'm' THEN 'f'
+    ELSE 'm'
+END;
