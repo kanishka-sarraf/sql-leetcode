615898c1103db8b3f819c2a720f40f10a8727037
kanishka-sarraf <kanishkasarraf20@gmail.com>
2025-09-09 15:05:58 +0530

    Time: 782 ms (47.42%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+/*
+-- solution1
 with max_department_salary as (
     select e.name as employee,
         d.name as department,
@@ -11,3 +13,13 @@ with max_department_salary as (
 select department, employee, salary 
 from max_department_salary
 where salary = max_salary;
+*/
+
+select department, employee, salary
+from (
+    select d.name as department, e.name as employee, e.salary,
+    dense_rank() over(partition by d.id order by e.salary desc) as rnk
+    from employee e
+    join department d on e.departmentid = d.id
+) t 
+where t.rnk = 1;
