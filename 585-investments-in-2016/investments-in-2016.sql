f12edd2e0e9394db5127b33aa24affba6f48d6bc
kanishka-sarraf <kanishkasarraf20@gmail.com>
2025-09-10 07:25:57 +0530

    Time: 469 ms (98.92%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,8 +1,25 @@
 # Write your MySQL query statement below
+/*
+-- solution1
 select round(sum(tiv_2016),2) as tiv_2016 from (
-select *, 
-count(*) over(partition by tiv_2015) as tiv_2015_count,
-count(*) over(partition by lat, lon) as lat_lon_count
-from insurance
+    select *, 
+    count(*) over(partition by tiv_2015) as tiv_2015_count,
+    count(*) over(partition by lat, lon) as lat_lon_count
+    from insurance
 ) t
 where tiv_2015_count > 1 and lat_lon_count = 1;
+*/
+
+select round(sum(tiv_2016),2) as tiv_2016
+from insurance 
+where 
+tiv_2015 in(
+    select tiv_2015 from insurance
+    group by tiv_2015
+    having count(*)>1
+) and 
+(lat, lon) in(
+    select lat, lon from insurance
+    group by lat, lon
+    having count(*)=1
+);
