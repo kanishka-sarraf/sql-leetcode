64213ca6a8f65cd6c4fd8f0618491a84a52007f0
kanishka-sarraf <kanishkasarraf20@gmail.com>
2025-09-12 09:15:17 +0530

    Time: 1027 ms (99.07%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -31,6 +31,7 @@ select title from movie_rating
 where rnk = 1;
 */
 
+/*
 select * from (
 select u.name as results
 from movierating mr
@@ -49,4 +50,27 @@ group by m.movie_id
 order by avg(rating) desc, m.title
 limit 1
 ) t2;
+*/
 
+-- Top user by number of ratings
+(
+    SELECT u.name AS results
+    FROM movierating mr
+    JOIN users u ON u.user_id = mr.user_id
+    GROUP BY u.user_id, u.name
+    ORDER BY COUNT(*) DESC, u.name
+    LIMIT 1
+)
+
+UNION ALL
+
+-- Top movie by average rating in Feb 2020
+(
+    SELECT m.title AS results
+    FROM movierating mr
+    JOIN movies m ON m.movie_id = mr.movie_id
+    WHERE mr.created_at >= '2020-02-01' AND mr.created_at < '2020-03-01'
+    GROUP BY m.movie_id, m.title
+    ORDER BY AVG(mr.rating) DESC, m.title
+    LIMIT 1
+);
