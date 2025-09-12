acab61d862b17a1c9377d31289b59152f3c0c97a
kanishka-sarraf <kanishkasarraf20@gmail.com>
2025-09-12 09:12:38 +0530

    Time: 1183 ms (69.13%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+/*
+-- solution1
 with user_movie_rating as (
     select m.*, u.*, mr.rating, mr.created_at
     from movierating mr
@@ -27,3 +29,24 @@ where rnk = 1
 union all
 select title from movie_rating
 where rnk = 1;
+*/
+
+select * from (
+select u.name as results
+from movierating mr
+join users u on u.user_id = mr.user_id
+group by u.user_id
+order by count(*) desc, u.name
+limit 1
+) t1
+union all
+select * from (
+select m.title
+from movierating mr
+join movies m on m.movie_id = mr.movie_id
+where date_format(created_at,'%Y-%m') = '2020-02'
+group by m.movie_id
+order by avg(rating) desc, m.title
+limit 1
+) t2;
+
